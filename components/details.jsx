f0b749f97cc6e1f4b617efd22acaa3517d9e3cd7
CodingLab-dz <codinglab001@gmail.com>
2025-02-16 16:46:08 +0100

    suspense2 correction1

--- components/details.jsx
+++ components/details.jsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { useState, useEffect, suspense } from "react";
+import React, { useState, useEffect, Suspense } from "react";
 import useEmblaCarousel from "embla-carousel-react";
 import Autoplay from 'embla-carousel-autoplay'
 import Image from "next/image";
